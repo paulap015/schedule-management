e2a49dcf0e1950406dc2d8622bbc0ef18d40fa38
Paula <ppena@unicauca.edu.co>
2022-07-20 16:16:12 -0500

    first commit

---
+++ src/test/java/co/unicauca/edu/schedule/ScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
